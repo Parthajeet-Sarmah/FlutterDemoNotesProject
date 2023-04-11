d1e71e36923f1e6ce35bc996cd50acb5358d2497
Parthajeet Sarmah <coderparth32768@gmail.com>
2023-04-11 20:23:35 +0530

    Removed local database CRUD notes. Added notes from Cloud Firestore.

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,15 @@
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+//Create
+class CouldNotCreateNoteException implements Exception {}
+
+//Read
+class CouldNotGetAllNotesException implements Exception {}
+
+//Update
+class CouldNotUpdateNoteException implements Exception {}
+
+//Delete
+class CouldNotDeleteNoteException implements Exception {}
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -101,6 +101,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my-id',
       isEmailVerified: false,
       email: 'foo@bar.com',
     );
@@ -122,6 +123,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'my_id',
       isEmailVerified: true,
       email: 'foo@bar.com',
     );
