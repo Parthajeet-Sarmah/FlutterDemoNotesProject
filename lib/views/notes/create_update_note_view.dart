d64693d1b87ab28f9c5ce3ebd782a7e442d46775
Parthajeet Sarmah <coderparth32768@gmail.com>
2023-04-18 09:35:49 +0530

    Added sharing notes functionality

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -5,6 +5,9 @@ import 'package:demonotes/services/crud/notes_service.dart';
 import 'package:demonotes/utilities/generics/get_arguments.dart';
 import 'package:flutter/material.dart';
 import 'package:demonotes/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
+
+import '../../utilities/dialogs/cannot_share_empty_note_dialog.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -90,9 +93,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('New Note'),
-        ),
+        appBar: AppBar(title: const Text('New Note'), actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ]),
         body: FutureBuilder(
             future: createOrGetExistingNote(context),
             builder: (context, snapshot) {
